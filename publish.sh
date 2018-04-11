8f0b5c72e550ca744767bfe949abb7e5abad46bb
Thiago Hersan <thiago.hersan+github@gmail.com>
2018-04-11 03:11:03 -0300

    travis publishing

---
+++ publish.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+git checkout --orphan gh-pages
+git rm --cached -r .
+
+./node_modules/\@angular/cli/bin/ng build --prod --base-href /auras-photo-app/
+ls -la | grep -v "\(dist\|CNAME\|.git\|publish.sh\)" | xargs rm -rf
+
+cp -r dist/* .
+rm -rf dist/
+
+git add .
+git commit -m "updates site"
+git push origin :gh-pages
+git push -u origin gh-pages
+
+git rm -rf *
+git checkout -f master
+git branch -D gh-pages
