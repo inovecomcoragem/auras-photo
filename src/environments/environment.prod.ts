4c98232f93ce4fde2cf10f7d11f2f83587130646
Thiago Hersan <thiago.hersan+github@gmail.com>
2018-04-21 11:53:00 -0300

    changes su-auras repo address

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   sensorURL: 'https://auras-sensor-server.herokuapp.com',
   backendURL: 'https://su-auras.herokuapp.com',
-  resultURL: 'https://caiobsouza.github.io/su-auras/result'
+  resultURL: 'https://inovecomcoragem.github.io/su-auras/result'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   sensorURL: 'https://auras-sensor-server.herokuapp.com',
   backendURL: 'https://su-auras.herokuapp.com',
-  resultURL: 'https://caiobsouza.github.io/su-auras/result'
+  resultURL: 'https://inovecomcoragem.github.io/su-auras/result'
 };
