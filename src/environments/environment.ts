629ceafb5a334db095ffe26651e8b1d902515843
Thiago Hersan <thiago.hersan+github@gmail.com>
2018-06-12 17:58:16 -0300

    changes backend api address

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   sensorURL: 'https://auras-sensor-server.herokuapp.com',
-  backendURL: 'https://su-auras.herokuapp.com',
-  resultURL: 'https://inovecomcoragem.github.io/su-auras/result'
+  backendURL: 'https://auras-app.herokuapp.com',
+  resultURL: 'https://inovecomcoragem.github.io/auras-app/result'
 };
